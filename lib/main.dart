1b6d811ebc69644450c734347bd0464e21e5910f
lenny1400 <eliasjonas001@gmail.com>
2021-10-12 18:09:09 +0200

    Update main.dart
    Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:simple_nav_bar/nav.dart';
+import 'package:simple_nav_bar/screens/login/login_register.dart';
 import 'package:simple_nav_bar/themes.dart';
 
 Future main()async {
@@ -39,37 +40,11 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bottom Navigation Bar Tutorial',
-      home: Nav(),
-      //home: LoadUpScreen(),
+      //home: Nav(),
+      home: LoginRegister(),
       theme: CustomTheme.lightTheme,
       darkTheme: CustomTheme.darkTheme,
       themeMode: currentTheme.currentTheme,
-    );
-  }
-}
-
-class LoadUpScreen extends StatefulWidget {
-  const LoadUpScreen({Key? key}) : super(key: key);
-
-  @override
-  _LoadUpScreenState createState() => _LoadUpScreenState();
-}
-
-class _LoadUpScreenState extends State<LoadUpScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      body: Center(
-        child: FittedBox(
-          fit: BoxFit.fitHeight,
-          child: Container(
-            height: MediaQuery.of(context).size.height*1,
-            width: MediaQuery.of(context).size.width*1,
-            color: theme.primaryColor,
-          ),
-        ),
-      ),
     );
   }
 }
