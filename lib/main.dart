20ba8356ca47d3b6e9b3bd665dd08d7473b132b6
lenny1400 <eliasjonas001@gmail.com>
2021-10-29 14:36:19 +0200

    Functional State of App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:simple_nav_bar/language.dart';
 import 'package:simple_nav_bar/nav.dart';
 import 'package:simple_nav_bar/screens/login/login_register.dart';
 import 'package:simple_nav_bar/themes.dart';
-
 
 Future main()async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,11 +37,9 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bottom Navigation Bar Tutorial',
       //home: Nav(),
@@ -52,11 +47,6 @@ class _MyAppState extends State<MyApp> {
       theme: CustomTheme.lightTheme,
       darkTheme: CustomTheme.darkTheme,
       themeMode: currentTheme.currentTheme,
-
-      translations: Languages(), // your translations
-      locale: Locale('en','US'), // translations will be displayed in that locale
-      fallbackLocale: Locale('en','GB'), // specify the fallback locale in case an invalid locale is selected.
-
     );
   }
 }
